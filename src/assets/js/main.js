4a60c3bf3640fcb296c935a9377543b051b82490
Driton007 <59842795+Driton007@users.noreply.github.com>
2021-03-21 22:41:27 +0100

    responsive menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,3 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
         elem.removeAttribute("data-src");
     });
 }, false);
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click',()=> {
+    if (menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+    
+})
